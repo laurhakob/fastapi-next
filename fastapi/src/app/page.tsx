b3cbe57dbddfe8b8f166af76d461badbe008b4fd
laurhakob <laurhakob@gmail.com>
2024-08-17 11:33:28 +0400

    first commit

---
+++ fastapi/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [input, setInput] = useState<string>('')
+  return (
+   <div></div>
+  );
+}
