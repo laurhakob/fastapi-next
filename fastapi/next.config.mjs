a5bd99890d432aa6a58c2ec2abe3a1c5e842e8b7
laurhakob <laurhakob@gmail.com>
2024-08-20 21:14:26 +0400

    Update next.config.js for Cloudflare deployment

--- fastapi/next.config.mjs
+++ fastapi/next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+  },
+};
 
 export default nextConfig;
